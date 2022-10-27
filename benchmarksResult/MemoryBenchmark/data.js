5419b9d3defc15aeba1afb5bb7a525d447dab950
github-action-benchmark <github@users.noreply.github.com>
2022-10-27 15:25:36 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 2a8db865e6b1716d79395c01e4abd2c1a5a288e6

---
+++ benchmarksResult/MemoryBenchmark/data.js
@@ -0,0 +1,86 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1666884335994,
+  "repoUrl": "https://github.com/H01001000/discordeno",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "heiheiho000@gmail.com",
+            "name": "H01001000",
+            "username": "H01001000"
+          },
+          "committer": {
+            "email": "heiheiho000@gmail.com",
+            "name": "H01001000",
+            "username": "H01001000"
+          },
+          "distinct": true,
+          "id": "2a8db865e6b1716d79395c01e4abd2c1a5a288e6",
+          "message": "Merge branch 'benchmark' of https://github.com/H01001000/discordeno into benchmark",
+          "timestamp": "2022-10-27T23:23:26+08:00",
+          "tree_id": "8ebf0047db4f526fab090d1fa12f2ef6813c7fa0",
+          "url": "https://github.com/H01001000/discordeno/commit/2a8db865e6b1716d79395c01e4abd2c1a5a288e6"
+        },
+        "date": 1666884333699,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "[Guild.toggles.features - Current] Get the features of a guild",
+            "value": 1820,
+            "unit": "ns/iter",
+            "range": 80
+          },
+          {
+            "name": "[Guild.toggles.features - Previous] Get the features of a guild",
+            "value": 1810,
+            "unit": "ns/iter",
+            "range": 270
+          },
+          {
+            "name": "[Transformer - Current] Discord User to a User",
+            "value": 145,
+            "unit": "ns/iter",
+            "range": 9
+          },
+          {
+            "name": "[Transformer - Previous] Discord User to a User",
+            "value": 145,
+            "unit": "ns/iter",
+            "range": 7
+          },
+          {
+            "name": "[Transformer - Current] User to a Discord User",
+            "value": 153,
+            "unit": "ns/iter",
+            "range": 9
+          },
+          {
+            "name": "[Transformer - Previous] User to a Discord User",
+            "value": 153,
+            "unit": "ns/iter",
+            "range": 8
+          },
+          {
+            "name": "[Cache Plugin Memory Usage After Loaded DB] - RSS",
+            "value": 275.94,
+            "unit": "MB",
+            "range": "273.81 … 277.78"
+          },
+          {
+            "name": "[Cache Plugin Memory Usage After Loaded DB] - Heap Used",
+            "value": 259.67,
+            "unit": "MB",
+            "range": "255.42 … 265.19"
+          },
+          {
+            "name": "[Cache Plugin Memory Usage After Loaded DB] - Heap Total",
+            "value": 275.8,
+            "unit": "MB",
+            "range": "273.04 … 277.81"
+          }
+        ]
+      }
+    ]
+  }
+}
