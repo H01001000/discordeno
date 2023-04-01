db33cbdb7d6bb1574e4f42b46ad1adcff63fac5a
Jonathan Ho <heiheiho000@gmail.com>
2023-04-01 10:59:11 +0800

    Update dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,6 +4,6 @@ danger.git.commits.forEach((commit) => {
   if (!commit.message.match(/^(build\()|(ci: )|(docs\()|(feat\()|(fix\()|(perf\()|(refactor\()|(test\()/g)) {
     warn(`Commit message '${commit.message}' does match the correct format`)
   }
-})
+}) 
 
 message(Date.now().toString())
